01fc13b4df85fb1dc799f768951735f73052f474
Nikros106 <nikhilmaithani29@gmail.com>
2026-08-02 11:55:25 +0530

    learn array

---
+++ arrya/push_pop_more/hello.js
@@ -0,0 +1,83 @@
+function len (){
+    var fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+var size = fruits.length;
+console.log(size)
+
+}
+function stringg (){
+  var fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+var list = fruits.toString();
+console.log(list)
+
+}
+function att (){
+ var fruits = ["Banana", "Orange", "Apple", "Mango"];
+var gat = fruits.at(2);
+console.log(gat)
+
+}
+function join1 (){
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+let sub = fruits.join(" * ");
+console.log(sub)
+
+}
+function pop2 (){
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+let fruit = fruits.pop();
+console.log(sub1)
+
+}
+function sift1 (){
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+let sub2 = fruits.shift();
+console.log(sub2)
+
+}
+function unsif1 (){
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+let sub3 =fruits.unshift("Lemon");
+console.log(sub3)
+
+}
+// function aina1 (){
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// delete fruits[0];
+// console.log(sub4)
+
+// }
+function con1 (){
+const myGirls = ["Cecilie", "Lone"];
+const myBoys = ["Emil", "Tobias", "Linus"];
+
+const sub5 = myGirls.concat(myBoys);
+console.log(sub5)
+
+}
+// function coup1 (){
+    // const fruits = ["Banana", "Orange", "Apple", "Mango"];
+    //   fruits.copyWithin(2, 0);
+    // console.log(sub5)
+    
+    // }
+
+    function flat1 (){
+   const myArr = [[1,2],[3,4],[5,6]];
+const sub6 = myArr.flat();
+    console.log(sub6)
+    
+    }
+    function sli1 (){
+   const fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+const sub7 = fruits.slice(1);
+    console.log(sub7)
+    
+    }
+    function sli12 (){
+const months = ["Jan", "Feb", "Mar", "Apr"];
+const sub8 = months.toSpliced(0, 1);
+    console.log(sub8)
+    
+    }
